dec41455798efccd88d70ca4b77905798539b984
Malena de Arriba <dearribamalena@gmail.com>
2023-03-14 15:10:44 -0300

    ejercicio 9 finalizado

---
+++ js/ejercicio9.js
@@ -0,0 +1,16 @@
+for(let numero = 1; numero <= 500; numero++){
+    document.write(numero);
+
+    if (numero % 4 === 0) {
+        document.write("es multiplo de 4");
+    } 
+    
+    if (numero % 9 === 0){
+        document.write("es multiplo de 9");
+    }
+    document.write("<br>");
+
+    if (numero % 5 === 0){
+        document.write("<hr>");
+    }
+}
